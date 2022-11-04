bd91841a55777a016cd43dad3402a9acbffada62
martDev2020 <sergiosoy2220@gmail.com>
2022-11-04 14:49:28 -0600

    Hola3

--- app.js
+++ app.js
@@ -11,13 +11,13 @@ const app = express();
 const wtpp_router = require('./routers/wttp');
 
 mongoose.connect('mongodb://127.0.0.1:27017/wtapp', (err, res) => {
-  if (err) {
-    throw err;
-  } else {
-    app.listen(port, function () {
-      console.log('Servidor corriendo en ' + port);
-    });
-  }
+    if (err) {
+        throw err;
+    } else {
+        app.listen(port, function () {
+            console.log('Servidor corriendo en ' + port);
+        });
+    }
 });
 /**=================================================================
  * PARSEO DE DATOS
@@ -31,25 +31,24 @@ app.use(bodyparser.json({ limit: '50mb', extended: true }));
  ===================================================================*/
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Access-Control-Allow-Request-Method');
-  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
-  res.header('Allow', 'GET, PUT, POST, DELETE, OPTIONS');
-  next();
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+    res.header('Allow', 'GET, PUT, POST, DELETE, OPTIONS');
+    next();
 });
-
-app.post('/api/receive_sandbox', async function (req, res) {
-  console.log(req.body);
-
-})
 
 app.use('/', express.static('client', { redirect: false }));
 app.use('/api', wtpp_router);
 app.get('*', function (req, res, next) {
-  res.sendFile(path.resolve('client/index.html'));
+    res.sendFile(path.resolve('client/index.html'));
+})
+
+app.post('api/receive_sandbox', async function (req, res) {
+    console.log(req.body);
+
 })
 /**=================================================================
  * SE EXPOrTA LA INSTANCIA APP
  ===================================================================*/
 module.exports = app;
-
